d5997c251786b6b6aeaa37a1d81a0591a805d792
gofastgirl <79942033+gofastgirl@users.noreply.github.com>
2026-01-08 18:50:30 -0700

    Replace sensitive credentials with placeholder values
    Updated sensitive information with placeholder values.

--- Platforms/Secrets.cs
+++ Platforms/Secrets.cs
@@ -1,18 +1,18 @@
 public static class Secrets
 {
     public static string PROXMOX_USER = "jadmin@pam";
-    public static string PROXMOX_PASSWORD = "0britishCO$$";
+    public static string PROXMOX_PASSWORD = "password";
 
     public static string OPNSENSE_KEY =
-        "rx9kjjjPjeGbRYwfSQVuI/k2VzhdocLJ4/LeS5mkJy5gKJqVYRYHClg2tpNmZm6D6JkRjjTG+QbSEQX2";
+        "key";
     public static string OPNSENSE_SECRET =
-        "HeBVHTYySIdHlOXyhJJQOvaWLrFKSRMFWf6qDm6hxVK7Bf5tbgafSsOg36nnkVlgPoBoiFJj4DPeb16t";
+        "secret";
     public static string OPNSENSE_USER = "jadmin@pam";
-    public static string OPNSENSE_PASSWORD = "0britishCO$$";
+    public static string OPNSENSE_PASSWORD = "password";
 
     public static string DOCKER_AGENT = Constants.strEMPTY;
     public static string DOCKER_USER = "jadmin";
-    public static string DOCKER_PASSWORD = "0britishCO$$";
+    public static string DOCKER_PASSWORD = "password";
 
     public static string ESXI8_KEY = Constants.strEMPTY;
     public static string ESXI8_USER = "root";
@@ -21,6 +21,6 @@ public static class Secrets
     public static string LINKWARDEN_USER = Constants.strEMPTY;
 
     public static string KUMA_USER = "jadmin";
-    public static string KUMA_PASSWORD = "0britishCO$$";
-    public static string KUMA_KEY = "uk2_EXvgiU9pbkfQkTlVCnRkR9Dr1TIUsEZqqgI8fZ6b";
+    public static string KUMA_PASSWORD = "password";
+    public static string KUMA_KEY = "key";
 }
